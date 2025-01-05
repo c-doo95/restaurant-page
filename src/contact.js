4825c24b2508bd4302d114eee72ed814d820d60d
c-doo95 <116585057+c-doo95@users.noreply.github.com>
2025-01-05 15:58:22 -0500

    style contact module

--- src/contact.js
+++ src/contact.js
@@ -4,23 +4,30 @@ const genContact = () => {
   contentDiv.replaceChildren();
 
   const contactDiv = document.createElement("div");
+  contactDiv.id = 'contactDiv';
   const displayImg = new Image();
   displayImg.src =
     "https://images.unsplash.com/photo-1526069631228-723c945bea6b?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHJlc3RhdXJhbnR8ZW58MHx8MHx8fDI%3D";
   contactDiv.appendChild(displayImg);
 
   const contactBox = document.createElement("div");
+  contactBox.id = 'contactBox'
+
+  const div1 = document.createElement("div");
 
   const restoName = document.createElement("h1");
   restoName.textContent = "placeholder name";
-  contactBox.appendChild(restoName);
+  div1.appendChild(restoName);
   const streetAddress = document.createElement("p");
   streetAddress.textContent = "123 rue yourstreet";
-  contactBox.appendChild(streetAddress);
+  div1.appendChild(streetAddress);
+
+  contactBox.appendChild(div1);
 
   const contactInfo = document.createElement("div");
   const reservation = document.createElement("p");
-  reservation.textContent = "To book a reservattion inquire at :";
+  reservation.id = "reservation";
+  reservation.textContent = "To book a reservation inquire at:";
   contactInfo.appendChild(reservation);
   const phoneNumber = document.createElement("p");
   phoneNumber.textContent = "(123) 456-7890";
